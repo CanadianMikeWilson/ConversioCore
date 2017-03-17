4e2b0b2ffb46649d4715afdd45df45ff7dc54dd5
Mike Wilson <mwilson@brainfluid.ca>
2017-03-17 09:51:35 -0700

    Convert to csproj format

--- build.bat
+++ build.bat
@@ -5,9 +5,9 @@ if "%config%" == "" (
 )
 
 set version=
-if not "%BuildCounter%" == "" (
-   set version=--version-suffix ci-%BuildCounter%
-)
+REM if not "%BuildCounter%" == "" (
+REM    set version=--version-suffix ci-%BuildCounter%
+REM )
 
 REM (optional) build.bat is in the root of our repo, cd to the correct folder where sources/projects are
 REM cd MyLibrary
@@ -21,7 +21,7 @@ REM - Option 1: Run dotnet build for every source folder in the project
 REM   e.g. call dotnet build <path> --configuration %config%
 REM - Option 2: Let msbuild handle things and build the solution
 REM "%programfiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe" MyLibrary.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
-call dotnet build src\ConversioCore --configuration %config%
+call dotnet build ConversioCore.sln --configuration %config%
 if not "%errorlevel%"=="0" goto failure
 
 REM Unit tests
@@ -31,7 +31,7 @@ REM if not "%errorlevel%"=="0" goto failure
 
 REM Package
 mkdir %cd%\Artifacts
-call dotnet pack src\ConversioCore --configuration %config% %version% --output Artifacts
+call dotnet pack src\ConversioCore\ConversioCore.csproj --configuration %config% %version% --output Artifacts
 if not "%errorlevel%"=="0" goto failure
 
 :success
